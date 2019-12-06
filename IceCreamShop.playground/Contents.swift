20bd7a82571f703a538d30fb35e406f8faaaed5b
Skye Leahy <skyeleahy@protonmail.com>
2019-12-06 11:06:40 -0800

    final commit, some errors still

--- IceCreamShop.playground/Contents.swift
+++ IceCreamShop.playground/Contents.swift
@@ -5,52 +5,56 @@ struct Flavor {
   var rating : Int
 }
 
-enum Size {
-  case small : Double = 3.99
-  case medium : Double = 4.99
-  case large : Double = 5.99
+enum Size: Double {
+    case small = 3.99
+    case medium = 4.99
+    case large = 5.99
 }
 
 struct Cone {
-  let flavor : Flavor
-  let topping : String
-  let size : Size
+  var flavor : Flavor
+  var topping : String
+  var size : Size
+    
+    func eat() {
+      print("Mmm! I love eating \(self.flavor)")
+    }
 }
 
-func eat() {
-  print("Mmm! I love eating \(Cone.flavor)")
-}
 
-class IceCreamShop() {
-  var flavors : []
+class IceCreamShop {
+  var flavors : [Flavor]
   var sizes: [Size]
-  var toppings: [""]
-
-  init(flavors: [], sizes: [Size], toppings: [""]) {
+  var toppings: [String]
+    
+  init(flavors: [Flavor], sizes: [Size], toppings: [String]) {
       self.flavors = flavors
       self.sizes = sizes
       self.toppings = toppings
   }
 
-  var totalSales : Double
-
 }
+
+var totalSales : Double = 0.00
 
 func listTopFlavors(flavors: [Flavor]) {
     var string : String = "Our favorite flavors are "
     for flavor in flavors {
-      if flavor.rating > 4.0 {
+      if flavor.rating > 4 {
         string += "\(flavor.name) "
       }
     }
     print(string)
 }
 
-func orderCone(flavor: Flavor = Flavor(name: "Vanilla", rating = 1.0), topping: String = "Vanilla", size: Size = Size.large) {
+func orderCone(flavor: Flavor = Flavor(name: "Vanilla", rating: 1), topping: String = "Vanilla", size: Size = Size.large) -> Cone {
     let newCone = Cone(flavor: flavor, topping: topping, size: size)
-    iceCreamShop.totalSales += newCone.size
+    
+    if let size = newCone.size as Double? {
+        totalSales += size
+    }
 
-    if let topping = newCone.flavor? {
+    if let topping = newCone.flavor as Flavor? {
       print("Your \(newCone.flavor) ice cream with \(topping) is \(newCone.size)")
     } else {
       print("Your \(newCone.flavor) ice cream is \(newCone.size)")
@@ -59,9 +63,9 @@ func orderCone(flavor: Flavor = Flavor(name: "Vanilla", rating = 1.0), topping: 
     return newCone
 }
 
-var vanilla = Flavor(name: "Vanilla", rating: 3.0)
-var chocolate = Flavor(name: "Chocolate", rating: 4.0)
-var strawberry = Flavor(name: "Strawberry", rating: 5.0)
+var vanilla = Flavor(name: "Vanilla", rating: 3)
+var chocolate = Flavor(name: "Chocolate", rating: 4)
+var strawberry = Flavor(name: "Strawberry", rating: 5)
 
 var arraySizes = [Size.small, Size.medium, Size.medium, Size.small, Size.large, Size.large, Size.small]
 var arrayToppings = ["Cupcake", "Vanilla", "Chocolate", "Peanut Butter", "Mango Dragonfruit", "Cinnamon Dolce"]
